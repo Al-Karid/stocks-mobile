982e065a84721966816af49c281b6fc812dbd69b
Al-Karid <grencisse@hotmail.com>
2025-04-01 19:51:55 +0000

    fix : swipe action bug

--- components/portfolio/PortfolioHolding.tsx
+++ components/portfolio/PortfolioHolding.tsx
@@ -29,39 +29,37 @@ export default function PortfolioHolding({ item }: PortfolioHoldingProps) {
   );
 
   return (
-    <Swipeable renderRightActions={renderRightActions}>
-      <View style={styles.holdingCard}>
-        {/* Left Section - Stock Info */}
-        <View style={styles.infoContainer}>
-          <Text style={styles.holdingTitle}>{item.symbol}</Text>
-          <Text style={styles.holdingText}>Quantity: {item.quantity}</Text>
-          <Text style={styles.holdingText}>
-            Buy Price: {item.averagePurchasePrice} FCFA
-          </Text>
-          <Text style={styles.holdingText}>
-            Current Price: {item.currentPrice} FCFA
-          </Text>
-        </View>
-
-        {/* Separator */}
-        <View style={styles.separator} />
-
-        {/* Right Section - Performance */}
-        <View
-          style={[
-            styles.performanceContainer,
-            item.performence >= 0
-              ? styles.positiveBackground
-              : styles.negativeBackground,
-          ]}
-        >
-          <Text style={styles.performanceText}>
-            {item.performence.toFixed(2)}%
-          </Text>
-          <Text style={styles.currentPrice}>{item.currentPrice} FCFA</Text>
-        </View>
+    <View style={styles.holdingCard}>
+      {/* Left Section - Stock Info */}
+      <View style={styles.infoContainer}>
+        <Text style={styles.holdingTitle}>{item.symbol}</Text>
+        <Text style={styles.holdingText}>Quantity: {item.quantity}</Text>
+        <Text style={styles.holdingText}>
+          Buy Price: {item.averagePurchasePrice} FCFA
+        </Text>
+        <Text style={styles.holdingText}>
+          Current Price: {item.currentPrice} FCFA
+        </Text>
       </View>
-    </Swipeable>
+
+      {/* Separator */}
+      <View style={styles.separator} />
+
+      {/* Right Section - Performance */}
+      <View
+        style={[
+          styles.performanceContainer,
+          item.performence >= 0
+            ? styles.positiveBackground
+            : styles.negativeBackground,
+        ]}
+      >
+        <Text style={styles.performanceText}>
+          {item.performence.toFixed(2)}%
+        </Text>
+        <Text style={styles.currentPrice}>{item.currentPrice} FCFA</Text>
+      </View>
+    </View>
   );
 }
 
