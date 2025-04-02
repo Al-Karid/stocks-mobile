d0b0ed12653a3be4aad83a8eb6342e2a9be8f86b
Al-Karid <grencisse@hotmail.com>
2025-04-02 22:57:10 +0000

    chore : remove main tab view

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,32 +1,15 @@
-import { Tabs } from "expo-router";
-import { FontAwesome } from "@expo/vector-icons";
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarStyle: { paddingVertical: 10 }, // Added padding to tabs
-      }}
-    >
-      <Tabs.Screen 
-        name="(portfolio)" 
-        options={{ 
-          title: "Home",
-          headerShown: false,
-          tabBarIcon: ({ color, size }) => (
-            <FontAwesome name="home" size={size} color={color} />
-          ),
-        }} 
-      />
-      <Tabs.Screen 
+    <Stack>
+      <Stack.Screen 
         name="stocks" 
         options={{ 
           title: "Stocks",
-          tabBarIcon: ({ color, size }) => (
-            <FontAwesome name="font-awesome" size={size} color={color} />
-          ),
+          headerShown: true,
         }} 
       />
-    </Tabs>
+    </Stack>
   );
 }
